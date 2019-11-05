2973ede5e8eb71b4fb0becd78bf318c9b6f4bf5b
Andrew Nguyen <secretbetta@gmail.com>
2019-11-05 13:52:57 -0800

    Changed List of Members to Users

--- src/main/java/com/secretbetta/BASS/blackjack/BlackjackEvent.java
+++ src/main/java/com/secretbetta/BASS/blackjack/BlackjackEvent.java
@@ -6,10 +6,10 @@ import java.util.List;
 import javax.annotation.Nonnull;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.MessageReaction.ReactionEmote;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.react.PrivateMessageReactionAddEvent;
@@ -28,9 +28,9 @@ public class BlackjackEvent extends ListenerAdapter {
 	
 	private boolean game = false;
 	private Blackjack blackjack;
-	private List<Member> players;
+	private List<User> players;
 	private ArrayList<Message> privatemsg;
-	private int[] rounds; // TODO Max number of cards in hand = 5
+	// private int[] rounds;
 	private boolean[] stays;
 	private int[] sums;
 	private MessageChannel chnl;
@@ -41,13 +41,13 @@ public class BlackjackEvent extends ListenerAdapter {
 	 * @param players Number of players
 	 * @param users   List of plauers
 	 */
-	public void newGame(int players, List<Member> users) {
+	public void newGame(int players, List<User> users) {
 		this.blackjack = new Blackjack(players);
 		this.players = users;
 		this.game = true;
 		this.blackjack.shuffle();
 		this.blackjack.dealAll(2);
-		this.rounds = new int[players];
+		// this.rounds = new int[players];
 		this.stays = new boolean[players];
 		this.chnl = null;
 		this.sums = new int[players];
@@ -65,24 +65,25 @@ public class BlackjackEvent extends ListenerAdapter {
 		 * Main game command
 		 */
 		if (content.startsWith("~~blackjack") && !game) {
-			if (message.getMentionedMembers().size() > 0) { // Checks if multiplayer
-				List<Member> players = new ArrayList<>();
-				players.add(message.getMember());
-				players.addAll(message.getMentionedMembers());
+			if (message.getMentionedMembers().size() > 0 && message.getMentionedMembers().size() <= 8) { // Checks if
+																											// multiplayer
+				List<User> players = new ArrayList<>();
+				players.add(message.getAuthor());
+				players.addAll(message.getMentionedUsers());
 				newGame(players.size(), players);
 				channel.sendMessage("Welcome to BlackJack! The players are:\n").queue();
 				int p = 0;
 				
 				// Sends msg to each player
-				for (Member player : players) {
-					channel.sendMessage(String.format("%s\n", player.getNickname())).queue();
+				for (User player : players) {
+					channel.sendMessage(String.format("%s\n", player.getName())).queue();
 					this.sums[p] = blackjack.addCards(p);
 					
 					EmbedBuilder emb = new EmbedBuilder();
 					emb.setTitle("BlackJack");
 					emb.setDescription("**Cards** " + this.blackjack.getHand(p).toString());
 					emb.addField("", "Click :one: to hit. Click :two: to stay.", false);
-					player.getUser().openPrivateChannel().complete().sendMessage(emb.build()).queue();
+					player.openPrivateChannel().complete().sendMessage(emb.build()).queue();
 					p++;
 				}
 			} else {
@@ -102,7 +103,7 @@ public class BlackjackEvent extends ListenerAdapter {
 			channel.sendMessage(this.blackjack.winner()).queue(); // TODO Might need a new method in blackjack as a win
 																	// method
 			for (int p = 0; p < this.players.size(); p++) {
-				this.chnl.sendMessage(String.format("%s's Hand: %s", this.players.get(p).getNickname(),
+				this.chnl.sendMessage(String.format("%s's Hand: %s", this.players.get(p).getName(),
 					this.blackjack.getHand(p).toString())).queue(); // TODO Embed!!!
 			}
 			game = false;
@@ -128,15 +129,7 @@ public class BlackjackEvent extends ListenerAdapter {
 			return;
 		}
 		
-		boolean flag = false;
-		
-		for (Member mmb : this.players) { // TODO Change this.players to list of Users and not Members
-			if (mmb.getId().equals(event.getUser().getId())) {
-				flag = true;
-			}
-		}
-		
-		if (game && flag) {
+		if (game && this.players.contains(event.getUser())) {
 			ReactionEmote choice = event.getReactionEmote();
 			
 			/**
@@ -179,13 +172,9 @@ public class BlackjackEvent extends ListenerAdapter {
 					}
 				}
 			} else if (choice.getAsCodepoints().equals("U+32U+20e3")) { // Stay
-				for (int x = 0; x < this.players.size(); x++) { // TODO This could probably be in a method or something
-					// Also I could probably embed this...
-					if (this.players.get(x).getId().equals(event.getUser().getId())) {
-						this.stays[x] = true;
-						this.chnl.sendMessage(String.format("%s is ready.", event.getUser().getAsMention())).queue();
-						break;
-					}
+				if (this.players.contains(event.getUser())) {
+					this.stays[this.players.indexOf(event.getUser())] = true;
+					this.chnl.sendMessage(String.format("%s is ready.", event.getUser().getAsMention())).queue();
 				}
 			}
 		}
