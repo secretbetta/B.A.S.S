f533bc3319ff8239d9d2ab107502dda439d20618
Andrew Nguyen <secretbetta@gmail.com>
2019-11-30 18:44:33 -0800

    Serverinfo Command

---
+++ src/main/java/com/secretbetta/BASS/Minecraft/ServerInfo.java
@@ -0,0 +1,19 @@
+package com.secretbetta.BASS.Minecraft;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class ServerInfo extends Command {
+	
+	public ServerInfo() {
+		this.cooldown = 5;
+		this.help = "Gets Minecraft server info";
+	}
+	
+	@Override
+	protected void execute(CommandEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
