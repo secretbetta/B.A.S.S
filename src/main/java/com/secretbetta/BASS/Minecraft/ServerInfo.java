8e07c35c93d9f761d3905b56935fd1c74d2c9b5d
Andrew Nguyen <secretbetta@gmail.com>
2019-11-30 18:51:52 -0800

    EmbedBuilder

--- src/main/java/com/secretbetta/BASS/Minecraft/ServerInfo.java
+++ src/main/java/com/secretbetta/BASS/Minecraft/ServerInfo.java
@@ -3,16 +3,32 @@ package com.secretbetta.BASS.Minecraft;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+
 public class ServerInfo extends Command {
 	
 	public ServerInfo() {
 		this.cooldown = 5;
 		this.help = "Gets Minecraft server info";
+		this.name = "mc";
 	}
 	
 	@Override
 	protected void execute(CommandEvent event) {
-		// TODO Auto-generated method stub
+		if (event.getAuthor().isBot()) {
+			return;
+		}
+		
+		/**
+		 * 
+		 */
+		EmbedBuilder info = new EmbedBuilder();
+		info.setTitle("A.S.S Minecraft Server");
+		info.setDescription("Dedicated Minecraft Server for A.S.S");
+		info.addField("", "", false);
+		info.addField("", "", false);
+		info.addField("", "", false);
+		info.addField("", "", false);
 		
 	}
 	
