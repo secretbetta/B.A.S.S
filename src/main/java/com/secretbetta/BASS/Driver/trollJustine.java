c6bd878ea3aa10a4021f64667dd20120521f3a43
Andrew Nguyen <secretbetta@gmail.com>
2020-05-14 18:25:32 -0700

    Just for lols

---
+++ src/main/java/com/secretbetta/BASS/Driver/trollJustine.java
@@ -0,0 +1,27 @@
+package com.secretbetta.BASS.Driver;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class trollJustine extends Command {
+	
+	public trollJustine() {
+		this.name = "troll";
+	}
+	
+	@Override
+	protected void execute(CommandEvent event) {
+		String[] args = event.getArgs().split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)");
+		args[1] = args[1].replace("\"", "");
+		int times = Integer.parseInt(args[0]);
+		for (int x = 0; x < times; x++) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			event.reply(args[1]);
+		}
+	}
+	
+}
