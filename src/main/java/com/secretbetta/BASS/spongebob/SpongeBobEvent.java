1390ad0882a43830fc974e4e2d44d29c7275d7f1
Andrew Nguyen <secretbetta@gmail.com>
2020-12-16 11:39:34 -0800

    Removing unused code

--- src/main/java/com/secretbetta/BASS/spongebob/SpongeBobEvent.java
+++ src/main/java/com/secretbetta/BASS/spongebob/SpongeBobEvent.java
@@ -1,22 +1,18 @@
 package com.secretbetta.BASS.spongebob;
 
-import java.io.IOException;
-
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class SpongeBobEvent extends ListenerAdapter {
 	
-	private Spongebob sb;
+	// private Spongebob sb;
 	
 	public SpongeBobEvent() {
-		try {
-			this.sb = new Spongebob();
-		} catch (IOException e) {
-			System.err.println("Could not load users into sb.");
-		}
+		// try {
+		// this.sb = new Spongebob();
+		// } catch (IOException e) {
+		// System.err.println("Could not load users into sb.");
+		// }
 	}
 	
 	@Override
@@ -25,10 +21,10 @@ public class SpongeBobEvent extends ListenerAdapter {
 			return;
 		}
 		
-		String id = event.getAuthor().getId();
-		MessageChannel channel = event.getChannel();
-		Message msg = event.getMessage();
-		String content = msg.getContentRaw();
+		// String id = event.getAuthor().getId();
+		// MessageChannel channel = event.getChannel();
+		// Message msg = event.getMessage();
+		// String content = msg.getContentRaw();
 		// if (this.trollIds.contains(id)) {
 		// channel.sendMessage(Spongebob.spongebobUpper(content)).queue();
 		// }
